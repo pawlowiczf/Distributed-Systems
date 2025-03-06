885304a166118c0106f45942635ae4dc938b1a0a
pawlowiczf <pawlowicz.filip137@gmail.com>
2025-03-06 20:24:11 +0100

    chat tcp

---
+++ 01-sockets/zad_domowe/client/client.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func RandomUsername() string {
+	names := []string{"marek", "adam", "filip", "kuba", "dorota", "kamil", "kacper", "andrzej", "tomek"}
+	digits := "0123456789"
+	randomDigits := make([]byte, 3)
+
+	for a := 0; a < 3; a++ {
+		randomDigits[a] = digits[rand.Intn(len(digits))]
+	}
+
+	return names[rand.Intn(len(names))] + string(randomDigits)
+}
+
+func main() {
+	chatID := flag.String("chatID", "aabbc", "Enter chatID to access the particular chat room")
+	address := flag.String("address", "127.0.0.2:9090", "Enter your source address IPv4 and port")
+	username := flag.String("username", RandomUsername(), "Enter your username")
+
+	flag.Parse()
+
+	localAddr, err := net.ResolveTCPAddr("tcp", *address)
+	if err != nil {
+		log.Fatalf("cannot resolve TCP IP addr: %s", err)
+	}
+
+	dialer := net.Dialer{
+		LocalAddr: localAddr,
+	}
+
+	fmt.Printf("Dialing server. My IP is: %s\n", localAddr.String())
+	fmt.Printf("You're (%s)\n", *username)
+
+	conn, err := dialer.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		log.Fatalf("cannot dial the server: %s", err)
+	}
+
+	defer conn.Close()
+	handleShutdownConn(conn, *username, *chatID)
+
+	go func() {
+		fmt.Fprintf(conn, "%s\n", *chatID)
+		fmt.Fprintf(conn, "%s\n", *username)
+
+		reader := bufio.NewReader(os.Stdin)
+
+		for {
+			messageText, err := reader.ReadString('\n')
+			if err != nil {
+				if err == io.EOF {
+					return
+				}
+				fmt.Println("couldn't read message from stdin: ", err)
+				continue
+			}
+
+			var message Message
+			message.Username = *username
+			message.Message = messageText
+
+			data, err := json.Marshal(message)
+			if err != nil {
+				fmt.Println("couldn't marshal message: ", err)
+				continue
+			}
+
+			data = append(data, '\n')
+			_, err = conn.Write(data)
+			if err != nil {
+				fmt.Println("couldn't send message to server: ", err)
+			}
+		}
+
+	}()
+
+	reader := bufio.NewReader(conn)
+	for {
+		messageText, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				return
+			}
+			fmt.Println("cannot read message from data: ", err)
+			return
+		}
+
+		var message Message
+		err = json.Unmarshal([]byte(messageText), &message)
+		if err != nil {
+			fmt.Print(messageText)
+			continue
+		}
+
+		fmt.Printf("[%s] (%s): %s", *chatID, message.Username, message.Message)
+	}
+}
+
+type Message struct {
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+
+type ShutdownConn struct {
+	Username string `json:"username"`
+	ChatID   string `json:"chat_id"`
+}
+
+func handleShutdownConn(conn net.Conn, username string, chatID string) {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-signalChan
+		message := ShutdownConn{
+			Username: username,
+			ChatID:   chatID,
+		}
+
+		data, err := json.Marshal(message)
+		if err != nil {
+			fmt.Println("couldn't marshal message: ", err)
+			return
+		}
+		data = append(data, '\n')
+		conn.Write(data)
+	}()
+}
---
+++ 01-sockets/zad_domowe/run.ps1
@@ -0,0 +1,12 @@
+cd ./server 
+Start-Process cmd -ArgumentList '/k go run server.go'
+Start-Sleep -Seconds 3
+cd ..
+Start-Process cmd -ArgumentList '/k go run client/client.go --address 127.0.0.2:9091'
+Start-Sleep -Seconds 2
+Start-Process cmd -ArgumentList '/k go run client/client.go --address 127.0.0.3:9092'
+
+Start-Process cmd -ArgumentList '/k go run client/client.go --address 127.0.0.4:9093 --chatID gjaua'
+Start-Process cmd -ArgumentList '/k go run client/client.go --address 127.0.0.5:9094 --chatID gjaua'
+
+
